294082cd75297228de4a0b142d9f23ea8ac8bf51
pratik8861074253@gmail.com <pratikvk2003@gmail.com>
2026-03-14 00:26:17 +0530

    small fixes in session controlller

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -104,9 +104,17 @@ export async function joinSession(req, res) {
     if (!session)
         return res.status(404).json({ message: "Session not found" });
 
+    if (session.status !== "active") {
+    return res.status(400).json({ message: "Cannot join a completed session" });
+    }
+
+    if (session.host.toString() === userId.toString()) {
+    return res.status(400).json({ message: "Host cannot join their own session as participant" });
+    }
+
     // check if session is already full - has a participant
     if (session.participant)
-        return res.status(404).json({ message: "Session is full" });
+        return res.status(409).json({ message: "Session is full" });
 
     session.participant = userId;
     await session.save();
@@ -141,9 +149,6 @@ export async function endSession(req, res) {
         return res.status(400).json({ message: "Session is already completed" });
     }
 
-    session.status = "completed";
-    await session.save();
-
     // delete stream video call
     const call = streamClient.video.call("default", session.callId);
     await call.delete({ hard: true });
@@ -151,6 +156,9 @@ export async function endSession(req, res) {
     // delete stream chat channel
     const channel = chatClient.channel("messaging", session.callId);
     await channel.delete();
+
+    session.status = "completed";
+    await session.save();
 
     res.status(200).json({ session, message: "Session ended successfully" });
 } catch (error) {
